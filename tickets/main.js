27db117c64c34b351bfe8ebdd6c39bbc4b95772d
dmitrysent <78569344+dmitrysent@users.noreply.github.com>
2021-06-08 00:10:15 +0300

    Add files via upload

---
+++ tickets/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function func(){
+    let reviewOfDeliveryOld = Number(document.getElementById("reviewOfDelivery").value),
+        reviewOfDeliveryNew = reviewOfDeliveryOld * 0.6,
+        emailOld = Number(document.getElementById("email").value),
+        emailNew = emailOld * 0.85,
+        chat = Number(document.getElementById("chat").value),
+        reviewNotPublicOld = Number(document.getElementById("reviewNotPublic").value),
+        reviewNotPublicNew = reviewNotPublicOld * 0.8,
+        jiraOld = Number(document.getElementById("jira").value),
+        jiraNew = jiraOld * 0.8,
+        questionsOld = Number(document.getElementById("questions").value),
+        questionsNew = questionsOld * 0.75,
+        reviewPublicOld = Number(document.getElementById("reviewPublic").value),
+        reviewPublicNew = reviewPublicOld * 1.2,
+        npsOld = Number(document.getElementById("nps").value),
+        npsNew = npsOld * 0.75,
+        callHelpOld = Number(document.getElementById("callHelp").value),
+        callHelpNew = callHelpOld * 0.85,
+        serviceContactOld = Number(document.getElementById("serviceContact").value),
+        serviceContactNew = serviceContactOld * 0.6,
+        resultOld = "Колличество контактов = " + (reviewOfDeliveryOld + chat + emailOld + reviewNotPublicOld + jiraOld + reviewPublicOld + npsOld + callHelpOld + serviceContactOld),
+        resultNew = "Колличество контактов с учётом коэффициента = " + (reviewOfDeliveryNew + chat + emailNew + reviewNotPublicNew + jiraNew + reviewPublicNew + npsNew + callHelpNew + serviceContactNew);
+
+    document.getElementById("resultOld").innerHTML = resultOld;
+    document.getElementById("resultNew").innerHTML = resultNew;
+};    
+    
